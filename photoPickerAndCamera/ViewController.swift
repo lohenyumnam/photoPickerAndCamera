a9a53a2207b7c869e6c4e665d0c0af60033342e3
Lohen Yumnam <lohen@lohens-macbook-pro.local>
2018-10-12 19:48:26 +0530

    Initial Commit

---
+++ photoPickerAndCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoPickerAndCamera
+//
+//  Created by Lohen Yumnam on 12/10/18.
+//  Copyright © 2018 Lohen Yumnam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
